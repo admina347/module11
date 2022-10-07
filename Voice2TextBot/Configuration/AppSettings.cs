e829e48f78ee286ec517a096151382fb6580d7d5
admina <it-mactep@mail.ru>
2022-10-07 12:54:23 +0500

    soft reset

--- Voice2TextBot/Configuration/AppSettings.cs
+++ Voice2TextBot/Configuration/AppSettings.cs
@@ -3,4 +3,7 @@ namespace Voice2TextBot.Configuration;
 public class AppSettings
 {
     public string BotToken { get; set; }
+    public string DownloadsFolder { get; set; }
+    public string AudioFileName { get; set; }
+    public string InputAudioFormat { get; set; }
 }
